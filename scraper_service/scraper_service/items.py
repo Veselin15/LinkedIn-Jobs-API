689923a56bc9225991f2bb510dde2a9154e581c6
veski4a <veskoveskov2006@gmail.com>
2026-01-11 15:31:12 +0200

    Refactor item model: create JobItem with detailed fields for job listings

--- scraper_service/scraper_service/items.py
+++ scraper_service/scraper_service/items.py
@@ -1,12 +1,14 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
-
-class ScraperServiceItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class JobItem(scrapy.Item):
+    title = scrapy.Field()
+    company = scrapy.Field()
+    location = scrapy.Field()
+    url = scrapy.Field()
+    posted_at = scrapy.Field()
+    description = scrapy.Field()
+    source = scrapy.Field()
+    skills = scrapy.Field()
+    salary_min = scrapy.Field()
+    salary_max = scrapy.Field()
+    currency = scrapy.Field()
