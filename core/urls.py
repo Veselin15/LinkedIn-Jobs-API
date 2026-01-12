81f98b72fb7d5b087f6ea1409dfebe52d3f60857
veski4a <veskoveskov2006@gmail.com>
2026-01-12 16:17:33 +0200

    Add job listing page and update navigation

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
+    path('jobs/', views.job_list, name='job_list'),
 ]
